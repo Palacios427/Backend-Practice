6508d29e1f8a2091c69e8c490d1fafa631f07e6f
Jorge Andrés Palacios Plascencia <A01722533@tec.mx>
2025-02-25 10:58:59 -0600

    Class finished

---
+++ controllers/items.controllers.js
@@ -0,0 +1,7 @@
+import {sqlConnect, sql} from "../utils/sql.js"
+
+export const getItems = async () => {
+    const pool = await sqlConnect();
+    const data = await pool.request().query("select * from items");
+    console.log(data);
+};
--- utils/sql.js
+++ utils/sql.js
@@ -4,14 +4,15 @@ const sqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
-  server: 'localhost',
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000
-  },
+  server: proccess.env.DB_SERVER,
   options: {
     encrypt: true, // for azure
     trustServerCertificate: false // change to true for local dev / self-signed certs
   }
 }
+
+const sqlConnect = async () => {
+    return await sql.connect(sqlConfig);
+};
+
+export { sqlConnect, sql };
