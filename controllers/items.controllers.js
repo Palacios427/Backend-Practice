823ec4de558729226cffc5e6dbf716d62b081045
Jorge Andrés Palacios Plascencia <A01722533@tec.mx>
2025-02-27 12:08:33 -0600

    post option created

--- controllers/items.controllers.js
+++ controllers/items.controllers.js
@@ -2,6 +2,18 @@ import {sqlConnect, sql} from "../utils/sql.js"
 
 export const getItems = async () => {
     const pool = await sqlConnect();
-    const data = await pool.request().query("select * from items");
+    const data = await pool.request().query('select * from items');
     console.log(data);
+};
+
+export const postItem = async (req, res) => {
+    const pool = await sqlConnect();
+    const data = await pool
+        .request()
+        .input("name", sql.VarChar, req.body.name)
+        .input("price", sql.Float, req.body.price)
+        .query("insert into items (name, price) values (@name, @price");
+    // console.log(data.recordset);
+    // res.json(data.recordset);
+    res.status(200).json({ operation: true });
 };
--- routes/items.routes.js
+++ routes/items.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getItems } from "../controllers/items.controllers.js";
+import { getItems, postItem } from "../controllers/items.controllers.js";
 
 const router = Router();
 
 router.get("/items/", getItems);
+// router.get("/items/:id", getItem);
+router.post("items/", postItem);
 
 
 export default router;
