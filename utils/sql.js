f166d6958f426697a7ac60fb9219785f2d660241
Jorge Andrés Palacios Plascencia <A01722533@tec.mx>
2025-02-27 11:21:32 -0600

    sql.js changed

--- utils/sql.js
+++ utils/sql.js
@@ -1,13 +1,13 @@
-const sql = require('mssql')
+import sql from "mssql";
 
 const sqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
-  server: proccess.env.DB_SERVER,
+  server: process.env.DB_SERVER,
   options: {
     encrypt: true, // for azure
-    trustServerCertificate: false // change to true for local dev / self-signed certs
+    trustServerCertificate: true // change to true for local dev / self-signed certs
   }
 }
 
