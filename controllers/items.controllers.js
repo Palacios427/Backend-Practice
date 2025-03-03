7f71c111df7eb641377d6ec14404df816ec8aaaf
Jorge Andrés Palacios Plascencia <A01722533@tec.mx>
2025-03-03 12:44:46 -0600

    Finished REST API

--- controllers/items.controllers.js
+++ controllers/items.controllers.js
@@ -10,21 +10,26 @@ export const getItem = async (req, res) => {
     const pool = await sqlConnect();
     const data = await pool
         .request()
-        .input("id", sql.Int, req.params.id)
-        .query("select * from items where id=@id");
-    res.status(200).json({ operation: true });
+        .input("myId", sql.Int, req.params.id)
+        .query("select * from items where id=@myId");
+    res.json(data.recordset);
 };
 
 export const postItem = async (req, res) => {
     const pool = await sqlConnect();
-    const data = await pool
+    await pool
         .request()
         .input("name", sql.VarChar, req.body.name)
         .input("price", sql.Float, req.body.price)
         .query("insert into items (name, price) values (@name, @price)");
-    // console.log(data.recordset);
-    // res.json(data.recordset);
-    res.status(200).json({ operation: true });
+
+    const data = await pool
+        .request()
+        .input("name", sql.VarChar, req.body.name)
+        .query("select * from items where name = @name");
+
+    console.log(data.recordset);
+    res.status(200).json({ operation: true, item:data.recordset[0]});
 };
 
 export const putItem = async (req, res) => {
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
 import itemsRoutes from "./routes/items.routes.js"
 import loginRoutes from "./routes/login.routes.js";
 
 const app = express();
 
+app.use(cors());
+app.use(morgan());
 app.use(express.json());
 app.use(indexRoutes);
 app.use(itemsRoutes);
