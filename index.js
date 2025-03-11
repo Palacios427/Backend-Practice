f0d0a5ca0aaf7b849ac7d473bd7549e8d703f775
Jorge Andrés Palacios Plascencia <A01722533@tec.mx>
2025-03-11 10:14:19 -0600

    Implemented Firebase DB

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
 import itemsRoutes from "./routes/items.routes.js"
 import items2Routes from "./routes/items2.routes.js"
+import items3Routes from "./routes/items3.routes.js"
 import loginRoutes from "./routes/login.routes.js";
 import { connectDB } from "./utils/mongodb.js";
 
 const app = express();
 
-connectDB();
+// connectDB();
 
 app.use(cors());
 app.use(morgan());
 app.use(express.json());
 app.use(indexRoutes);
 app.use(itemsRoutes);
-app.use(items2Routes);
+// app.use(items2Routes);
+app.use(items3Routes);
 app.use(loginRoutes);
 
 app.listen(5000, console.log("http://localhost:5000"));
